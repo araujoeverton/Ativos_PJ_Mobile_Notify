be8625b899d20539a67b7515cc6f4d426d3514ce
everton <everton.proposta@gmail.com>
2025-04-20 12:12:37 -0300

    atualizacao main sg

--- terraform/environments/homolog/modules/security_group/main.tf
+++ terraform/environments/homolog/modules/security_group/main.tf
@@ -2,33 +2,33 @@ resource "aws_security_group" "main" {
   name_prefix = "${var.name_prefix}-"
   vpc_id      = var.vpc_id
   description = var.description
+
   tags = {
-    Name = "${var.name_prefix}"
+    Name = var.name_prefix
   }
 }
 
 resource "aws_security_group_rule" "ingress" {
-  count             = length(var.rules)
+  count             = length(var.ingress_rules)
   security_group_id = aws_security_group.main.id
   type              = "ingress"
-  protocol          = element(var.rules, count.index).protocol
-  from_port         = element(var.rules, count.index).from_port
-  to_port           = element(var.rules, count.index).to_port
-  cidr_blocks       = lookup(element(var.rules, count.index), "cidr_blocks", null)
-  ipv6_cidr_blocks  = lookup(element(var.rules, count.index), "ipv6_cidr_blocks", null)
-  prefix_list_ids   = lookup(element(var.rules, count.index), "prefix_list_ids", null)
-  source_security_group_id = lookup(element(var.rules, count.index), "source_security_group_id", null)
+  protocol          = var.ingress_rules[count.index].protocol
+  from_port         = var.ingress_rules[count.index].from_port
+  to_port           = var.ingress_rules[count.index].to_port
+  cidr_blocks       = try(var.ingress_rules[count.index].cidr_blocks, null)
+  ipv6_cidr_blocks  = try(var.ingress_rules[count.index].ipv6_cidr_blocks, null)
+  prefix_list_ids   = try(var.ingress_rules[count.index].prefix_list_ids, null)
+  source_security_group_id = try(var.ingress_rules[count.index].source_security_group_id, null)
 }
 
 resource "aws_security_group_rule" "egress" {
-  count             = length(var.rules)
+  count             = length(var.egress_rules)
   security_group_id = aws_security_group.main.id
   type              = "egress"
-  protocol          = element(var.rules, count.index).protocol
-  from_port         = element(var.rules, count.index).from_port
-  to_port           = element(var.rules, count.index).to_port
-  cidr_blocks       = lookup(element(var.rules, count.index), "cidr_blocks", ["0.0.0.0/0"]) # Default para permitir todo o tráfego de saída
-  ipv6_cidr_blocks  = lookup(element(var.rules, count.index), "ipv6_cidr_blocks", ["::/0"]) # Default para permitir todo o tráfego de saída IPv6
-  prefix_list_ids   = lookup(element(var.rules, count.index), "prefix_list_ids", null)
-  destination_security_group_id = lookup(element(var.rules, count.index), "destination_security_group_id", null)
+  protocol          = var.egress_rules[count.index].protocol
+  from_port         = var.egress_rules[count.index].from_port
+  to_port           = var.egress_rules[count.index].to_port
+  cidr_blocks       = try(var.egress_rules[count.index].cidr_blocks, ["0.0.0.0/0"])
+  ipv6_cidr_blocks  = try(var.egress_rules[count.index].ipv6_cidr_blocks, ["::/0"])
+  prefix_list_ids   = try(var.egress_rules[count.index].prefix_list_ids, null)
 }
--- terraform/environments/homolog/modules/security_group/variables.tf
+++ terraform/environments/homolog/modules/security_group/variables.tf
@@ -1,31 +1,42 @@
 variable "name_prefix" {
-  type = string
-  description = "Prefixo para o nome do Security Group"
+  description = "Prefixo para nomear o security group"
+  type        = string
 }
 
 variable "vpc_id" {
-  type = string
-  description = "ID da VPC"
+  description = "ID da VPC onde o security group será criado"
+  type        = string
 }
 
 variable "description" {
-  type = string
-  description = "Descrição do Security Group"
-  default = "Managed by Terraform"
+  description = "Descrição do security group"
+  type        = string
 }
 
-variable "rules" {
+variable "ingress_rules" {
+  description = "Lista de regras de entrada (ingress)"
   type = list(object({
-    ingress = bool
-    protocol = string
-    from_port = number
-    to_port = number
-    cidr_blocks = optional(list(string))
-    ipv6_cidr_blocks = optional(list(string))
-    prefix_list_ids = optional(list(string))
+    protocol                  = string
+    from_port                = number
+    to_port                  = number
+    cidr_blocks              = optional(list(string))
+    ipv6_cidr_blocks         = optional(list(string))
+    prefix_list_ids          = optional(list(string))
     source_security_group_id = optional(string)
+  }))
+  default = []
+}
+
+variable "egress_rules" {
+  description = "Lista de regras de saída (egress)"
+  type = list(object({
+    protocol                      = string
+    from_port                    = number
+    to_port                      = number
+    cidr_blocks                  = optional(list(string), ["0.0.0.0/0"])
+    ipv6_cidr_blocks             = optional(list(string), ["::/0"])
+    prefix_list_ids             = optional(list(string))
     destination_security_group_id = optional(string)
   }))
-  description = "Lista de regras de entrada e saída"
   default = []
 }
